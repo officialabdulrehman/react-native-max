46f7d5cdadfa22fa67beca66a02ac243341cbf0c
NizTheDev <arehman08@hotmail.com>
2022-11-27 20:48:12 +0500

    feat: working on my first mobile app. seems fun

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Add your goal!' />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>Goals List</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  textInput: {
+    color: '#ccc',
+    borderWidth: 1
+  }
 });
